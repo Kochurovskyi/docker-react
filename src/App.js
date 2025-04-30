0d68d37bc3cbd907b14413b745e97251a3a52213
yuko <yuri.ko@pria.com>
2025-04-30 11:35:31 +0300

    3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>XXX! Fuck it !XXX</p>
+        <p>XXXZZ! Fuck it !XXXZZ</p>
         <a
           className="App-link"
           href="https://reactjs.org"
