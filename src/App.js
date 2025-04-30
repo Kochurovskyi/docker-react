46f5636f5acf848774d7649aca5c034eb9886ae2
yuko <yuri.ko@pria.com>
2025-04-30 13:41:19 +0300

    ci docker build with aws deploy - port fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>XXXZZ! Fuck it !XXXZZ</p>
+        <p>X Fuck it X</p>
         <a
           className="App-link"
           href="https://reactjs.org"
